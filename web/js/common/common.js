5cb36d9a403177315279e09451ae927095467594
user <user@Aspire-E-15-LSJ>
2018-12-17 17:51:45 +0900

    주문 메인 수정

--- web/js/common/common.js
+++ web/js/common/common.js
@@ -1,4 +1,5 @@
 //금액 천 단위로 콤마 찍기
-function comma(price){
-	return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+function numComma(num){
+	var numStr = String(num);
+	return numStr.replace(/(\d)(?=(?:\d{3})+(?!\d))/g,"$1,");
 }
