95995466b117c8902676a4e4577a2b3a113ca993
IAN <IAN@DESKTOP-072JAOU>
2019-01-08 14:49:20 +0900

    .

--- src/com/kh/semi/customer/board/controller/SelectNoticeListServlet.java
+++ src/com/kh/semi/customer/board/controller/SelectNoticeListServlet.java
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SelectNoticeBoardList
  */
-@WebServlet("/selectNoticeBoardList.bo")
-public class SelectNoticeBoardList extends HttpServlet {
+@WebServlet("/selectNoticeList.bo")
+public class SelectNoticeListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SelectNoticeBoardList() {
+    public SelectNoticeListServlet() { // 공지사항 조회
         super();
         // TODO Auto-generated constructor stub
     }
