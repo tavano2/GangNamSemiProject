00080611074400475bc14ce34b97f7fababbed27
IAN <IAN@DESKTOP-072JAOU>
2019-01-09 18:02:00 +0900

    .

--- src/com/kh/semi/customer/board/controller/UpdateNoticeBoardServlet.java
+++ src/com/kh/semi/customer/board/controller/UpdateNoticeBoardServlet.java
@@ -7,34 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UpdateNoticeBoardServlet
- */
+
 @WebServlet("/updateNoticeBoard.bo")
 public class UpdateNoticeBoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public UpdateNoticeBoardServlet() { // 게시글 수정
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
